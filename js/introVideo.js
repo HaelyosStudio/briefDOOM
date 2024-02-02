ad53687a7122fb74188aaf823e838ae864f8487c
HaelyosStudio <adam.han.martin@gmail.com>
2024-02-02 15:55:21 +0100

    update

---
+++ js/introVideo.js
@@ -0,0 +1,8 @@
+document.getElementById("introButton").addEventListener("click", function() {
+    var video = document.querySelector(".introVideo");
+    var button = document.getElementById("introButton");
+    video.classList.add("hidden");
+    button.classList.add("hidden");
+    });
+
+
